644ae1f351d1f444ab5a55932a5abe8fbda09451
waterada <w.terada@gmail.com>
2015-10-17 16:39:56 +0900

    セットアップなどを整備。

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,15 +3,15 @@ set -eu
 
 # httpd.conf 追加
 cd /etc/httpd/sites-enabled
-if [[ -f httpd-vhosts.reactjs-php.conf ]]; then
-    sudo rm httpd-vhosts.reactjs-php.conf
+if [[ -f httpd-vhosts.react-php.conf ]]; then
+    sudo rm httpd-vhosts.react-php.conf
 fi
-sudo cp /var/www/html/reactjs-php/setup/httpd-vhosts.conf httpd-vhosts.reactjs-php.conf
+sudo cp /var/www/html/react-php/setup/httpd-vhosts.conf httpd-vhosts.react-php.conf
 sudo /etc/init.d/httpd restart
 
 # テストコード
 echo "C:\Windows\System32\drivers\etc\hosts に下記を追記してください:"
-echo 192.168.33.10  reactjs-php.local
+echo 192.168.33.10  react-php.local
 echo
-echo "次のページにアクセスしてみてください: http://reactjs-php.local/"
+echo "次のページにアクセスしてみてください: http://react-php.local/"
 echo "監視ログ: tailf /etc/httpd/logs/error.log"
