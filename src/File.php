4101d3c926a5b082b680b093dbd02c97237efeb0
waterada <w.terada@gmail.com>
2015-10-21 11:47:56 +0900

    File.phpが漏れていたので追加。

---
+++ src/File.php
@@ -0,0 +1,12 @@
+<?php
+namespace waterada\ReactPHP;
+
+class File {
+    public static function load($path) {
+        return json_decode(file_get_contents($path), true);
+    }
+
+    public static function save($path, $contents) {
+        file_put_contents($path, json_encode($contents));
+    }
+}
